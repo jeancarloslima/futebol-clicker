e029ea08ab44300d27a1518b366e2d68a7256808
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-05-27 19:24:36 -0300

    Adicionando teste de melhorias

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const btnCliclador = document.getElementById("btn-clicador");
 const elementoContador = document.getElementById("contador");
 const elementoMultiplicador = document.getElementById("multiplicador");
+const melhorias = document.querySelectorAll(".container-melhorias-teste ul li button");
 let contador = 0;
 let multiplicador = 0.1;
 
 btnCliclador.addEventListener('click', () => {
-    contador+= multiplicador;
-    elementoContador.textContent = contador.toFixed(1);
+    contador += multiplicador;
+    atualizaElementos();
+});
+
+melhorias.forEach(melhoria => {
+    melhoria.addEventListener('click', () => {
+        multiplicador += Number(melhoria.value);
+        atualizaElementos();
+    })
 })
+
+function atualizaElementos() {
+    elementoContador.textContent = contador.toFixed(1);
+    elementoMultiplicador.textContent = "+ " + multiplicador;
+}
